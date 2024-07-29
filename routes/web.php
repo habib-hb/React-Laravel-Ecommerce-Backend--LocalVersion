0ce428a6fe636479f82ad4974e222bf590aba601
habib-hb <developerhabib1230@gmail.com>
2024-07-29 16:56:28 +0600

    Just Awesome

--- routes/web.php
+++ routes/web.php
@@ -117,3 +117,73 @@ Route::get('/storage/images/{file_name}', function ($file_name) {
     $response->header("Content-Type", $type);
     return $response;
 });
+
+
+
+// Review Message processing
+Route::post('api/dashboard/review-upload', function(Request $request){
+
+        //Validating the request inputs
+        $request->validate([
+            //formDataToSend.append('name', nameData ?? '');
+            // formDataToSend.append('user_id', userIdData  ?? '');
+            // formDataToSend.append('product_id', productIdData ?? '');
+            // formDataToSend.append('review_text', reviewTextData ?? '');
+            // formDataToSend.append('selected_stars', selectedStarsData ?? '');
+            // formDataToSend.append('review_image', reviewImageFile ?? '');
+
+            'name' => 'required',
+            'user_id' => 'required',
+            'product_id' => 'required',
+            'review_text' => 'required',
+            'selected_stars' => 'required',
+             ]);
+
+
+             // Declaring it beforehand to avoid any type of error if there's no image uploaded
+             $review_image_url=null;
+        if($request->hasFile('review_image')){
+        // Saving the review image in the local storage
+        $review_image = $request->review_image;
+        $review_image_name = time() . "--" . $review_image->getClientOriginalName();
+        $review_image->storeAs('images', $review_image_name, 'public');
+        // Review Image Url
+        $review_image_url = Storage::url('images/' . $review_image_name);
+        // Review Image Name without extension
+        $review_image_name_without_extension = pathinfo($review_image_name, PATHINFO_FILENAME);
+        }
+
+        //DB update or insert if the user doesn't exist
+        $availability_checking = DB::select('SELECT * from product_reviewers WHERE reviewer_id = ?', [$request->user_id]);
+
+        if(count($availability_checking) == 0){
+            $review_image_url ? DB::table('product_reviewers')->insert([
+                    'reviewer_name' => $request->name,
+                    'reviewer_id' => $request->user_id,
+                    'reviewer_avatar' => 'http://127.0.0.1:8000' . $review_image_url,
+                ]) : DB::table('product_reviewers')->insert([
+                    'reviewer_name' => $request->name,
+                    'reviewer_id' => $request->user_id,
+                ]);
+             }
+        else{
+
+           $review_image_url ? DB::table('product_reviewers')->where('reviewer_id', $request->user_id)->update([
+                'reviewer_name' => $request->name,
+                'reviewer_avatar' => 'http://127.0.0.1:8000' . $review_image_url,
+            ]) : null;
+
+        }
+
+        //DB operation for the review insertion
+        DB::table('product_reviews')->insert([
+            'product_id' => intval($request->product_id),
+            'reviewer_name' => $request->name,
+            'review' => $request->review_text,
+            'reviewer_id' => $request->user_id,
+            'rating' => intval($request->selected_stars),
+        ]);
+
+
+        return response()->json(['message' => 'File uploaded successfully'], 200);
+});
