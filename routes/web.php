c559deca246245e43d602f30eb5f4d63465cf797
habib-hb <developerhabib1230@gmail.com>
2024-08-02 18:25:05 +0600

    awesome

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Support\Facades\DB;
@@ -14,6 +15,16 @@ use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 
@@ -297,7 +308,11 @@ Route::post('api/dashboard/review-upload', function(Request $request){
 
 
         $user = User::where('id', $request->laravel_id)->first();
-        
+
         return response()->json(['email' => $user->email], 200);
 
     });
+
+
+
+require __DIR__.'/auth.php';
